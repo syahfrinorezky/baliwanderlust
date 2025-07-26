56699f63cba197b5115c9ed748f5a1a6997d0873
syahfrinorezky <syahfrinorezky@gmail.com>
2025-07-27 01:52:40 +0800

    feat: add theme toggle button

---
+++ src/components/common/themeToggle.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { Button } from "../ui/button";
+
+function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Button
+      variant={"outline"}
+      className="relative h-10 w-10 rounded-full cursor-pointer"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      aria-label="Toggle theme">
+      <FaSun className="absolute transition-all duration-300 ease-in-out rotate-0 scale-100 dark:rotate-90 dark:scale-0" />
+      <FaMoon className="absolute transition-all duration-300 ease-in-out rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
+    </Button>
+  );
+}
+
+export default ThemeToggle;
