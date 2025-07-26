bd334e87b5132bd8ae2fcc05240ff5798e632e08
syahfrinorezky <syahfrinorezky@gmail.com>
2025-07-27 01:54:12 +0800

    feat: add team member

---
+++ src/constants/teamMember.ts
@@ -0,0 +1,22 @@
+export const teamMember = [
+  {
+    name: "I Kadek Mahendra",
+    src: "/images/team/dummy-mahendra.png",
+    role: "Tour Manager",
+  },
+  {
+    name: "Ni Komang Rini",
+    src: "/images/team/dummy-rini.png",
+    role: "Customer Experience Lead",
+  },
+  {
+    name: "I Gede Aditya",
+    src: "/images/team/dummy-aditya.png",
+    role: "Digital Strategist",
+  },
+  {
+    name: "Ayu Lestari",
+    src: "/images/team/dummy-ayu.png",
+    role: "Travel Consultant",
+  },
+];
