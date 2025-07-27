af02a50955cea659dfa3a366fdbec1ff2644e852
syahfrinorezky <syahfrinorezky@gmail.com>
2025-07-27 17:53:16 +0800

    feat: add timeline for journey section

---
+++ src/constants/milestoneTl.ts
@@ -0,0 +1,25 @@
+import { Handshake } from "lucide-react";
+import { HiMagnifyingGlass, HiTrophy, HiUser } from "react-icons/hi2";
+
+export const milestonesTl = [
+  {
+    year: "2020",
+    title: "Bali Wanderlust Founded",
+    icon: HiMagnifyingGlass,
+  },
+  {
+    year: "2021",
+    title: "1,000+ Travelers Served",
+    icon: HiUser,
+  },
+  {
+    year: "2022",
+    title: "15+ Local Partnerships Established",
+    icon: Handshake,
+  },
+  {
+    year: "2023",
+    title: "Top 10 Bali Travel Startup",
+    icon: HiTrophy,
+  },
+];
