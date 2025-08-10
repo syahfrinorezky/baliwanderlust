5cad452c5d51477bd9cc0e4c6722c50b20764f01
syahfrinorezky <syahfrinorezky@gmail.com>
2025-08-10 17:35:13 +0700

    feat: add condition style especially at home page

--- src/components/common/themeToggle.tsx
+++ src/components/common/themeToggle.tsx
@@ -1,11 +1,16 @@
+/** @format */
+
 "use client";
 
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import clsx from "clsx";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { Button } from "../ui/button";
+import { usePathname } from "next/navigation";
 
 function ThemeToggle() {
+  const isHome = usePathname() === "/";
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
@@ -17,11 +22,16 @@ function ThemeToggle() {
 
   return (
     <Button
-      variant={"outline"}
-      className="relative h-10 w-10 rounded-full cursor-pointer"
+      variant={"ghost"}
+      className="relative h-10 w-10 rounded-full cursor-pointer bg-transparent hover:bg-transparent"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       aria-label="Toggle theme">
-      <FaSun className="absolute transition-all duration-300 ease-in-out rotate-0 scale-100 dark:rotate-90 dark:scale-0" />
+      <FaSun
+        className={clsx(
+          "absolute transition-all duration-300 ease-in-out rotate-0 scale-100 dark:rotate-90 dark:scale-0",
+          isHome ? "text-amber-300" : ""
+        )}
+      />
       <FaMoon className="absolute transition-all duration-300 ease-in-out rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
     </Button>
   );
